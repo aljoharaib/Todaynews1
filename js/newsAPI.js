48fa62abac235e140a894cf3a1419ed0c0ac409d
aljoharaib <88499465+aljoharaib@users.noreply.github.com>
2022-03-10 19:58:43 +0300

    newsd

--- js/newsAPI.js
+++ js/newsAPI.js
@@ -1,22 +1,24 @@
+
 console.log("here api");
 
 const newsKey ="e8cf524855c548afa31fc15f5d56cecb"
 
-const newUrl =` https://newsapi.org/v2/everything?q=bitcoin&apiKey=${newsKey}`
+let searchWord="apple"
+const newUrl =` https://newsapi.org/v2/everything?q=${searchWord}&apiKey=${newsKey}`
 
-let cat="general"
+let cat="health"
 let catApi=`https://newsapi.org/v2/top-headlines?apiKey=${newsKey}&category=${cat}`
 
 let headings=document.querySelectorAll("#headings a");
 for(let heading of headings){
     heading.addEventListener("click",(event) =>{
 cat=event.target.id;
-catApi=`https://newsapi.org/v2/top-headlines?category=${cat}&apiKey=${newsKey}`
-
-    })
+catApi=`https://newsapi.org/v2/top-headlines?category=${cat}&apiKey=${newsKey}&page=${page}`
+searchNews()  
+})
 }
-
-fetch( catApi)
+function  searchNews() {
+fetch(catApi)
 .then(response => response.json())
 .then(data => {
     console.log(data.articles)
@@ -42,3 +44,46 @@ document.getElementById("news").innerHTML = data.articles.map(news =>
 }
 
 );
+}
+searchNews()
+
+const ul=document.querySelector("ul");
+let allPages=15;
+function elem(allPages, page){
+let li='';
+let beforePages= page - 1;
+let afterPages=page + 1;
+let LiActive;
+if(page >1){
+  li +=`<li class="btn" onclick="elem(allPages, ${page-1})"><i class="fas fa-angle-left"></i></li>`;
+}
+
+for(let pageLength = beforePages; pageLength < afterPages; pageLength++){
+
+if(pageLength > allPages){
+  continue;
+}
+if(pageLength ==0){
+  pageLength= pageLength +1;
+}
+
+if(page ==  pageLength){
+  LiActive = 'active';
+}else{
+  LiActive ='';
+}
+
+  li +=`<li class="numb ${LiActive}" onclick="elem(allPages, ${pageLength})"><span>${pageLength}</span></li>`
+
+}
+
+if(page < allPages){
+  li +=`<li class="btn"onclick="elem(allPages, ${page+1})"><i class="fas fa-angle-right"></i></li>`;
+}
+ul.innerHTML=li;
+}
+elem(allPages,2)
+document.querySelector(".next").addEventListener("click" ,function () {
+  
+
+});
