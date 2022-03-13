c8ea2153100c778052785f88839435f874e4fb18
aljoharaib <88499465+aljoharaib@users.noreply.github.com>
2022-03-13 18:26:01 +0300

    newsd3

--- newsAPI.js
+++ newsAPI.js
@@ -47,13 +47,13 @@ document.getElementById("news").innerHTML = data.articles.map(news =>
 
 );
 }
+// search bar&btn
 document.querySelector("#btn-search").addEventListener("click", function(){
   searchWord=document.querySelector("#news-search").value
   console.log("Enter");
 console.log(searchWord);
 catApi=` https://newsapi.org/v2/everything?q=${searchWord}&apiKey=${newsKey}&page=${page}`
   searchNews();
-
 });
 // next
 document.querySelector("#next").addEventListener("click", function(){
@@ -65,13 +65,13 @@ console.log(disabled );
 }
 page++;
 console.log(page);
-catApi=`https://newsapi.org/v2/top-headlines?category=${cat}&apiKey=${newsKey}&page=${page}`
+catApi=`https://newsapi.org/v2/top-headlines?category=${cat}&apiKey=${newsKey}&page=${page}` 
 searchNews() 
 });
 // brev
 document.querySelector("#prev").addEventListener("click", function(){
   console.log("prev here");
-  if(page == 5 ){
+  if(page == 1 ){
     console.log("its p 5");
     prev.disabled = true;
 console.log(disabled);
